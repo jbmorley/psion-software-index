5d65af36b8c1bc2d7711f0ce863dd4a8b717b767
Jason Morley <hello@jbmorley.co.uk>
2024-10-18 13:37:02 -1000

    ci: Publish to self-hosted server

---
+++ ansible/deploy.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+
+ANSIBLE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIRECTORY = os.path.dirname(ANSIBLE_DIRECTORY)
+FILES_DIRECTORY = os.path.join(ROOT_DIRECTORY, "_site")
+
+
+def main():
+    os.chdir(ANSIBLE_DIRECTORY)
+    key_file = os.path.join(ANSIBLE_DIRECTORY, "ssh_key")
+    with open(key_file, "w") as fh:
+        fh.write(os.environ["ANSIBLE_SSH_KEY"])
+        fh.write("\n")
+    os.chmod(key_file, 0o400)
+    try:
+        subprocess.check_call(["ansible-playbook",
+                               "-vvvv",
+                               "-i", "hosts",
+                               "index.yml",
+                               "--extra-vars", f"root={FILES_DIRECTORY}",
+                               "--extra-vars", f"ansible_ssh_private_key_file={key_file}"])
+    finally:
+        os.remove(key_file)
+
+if __name__ == "__main__":
+    main()
